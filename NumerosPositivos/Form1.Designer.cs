908e29f12dba79fe9dd9ff52b2c5db498f476c9f
Sandra Aguilar <Sandra Aguilar@DESKTOP-MEVF2K3>
2024-05-11 00:12:50 -0600

    Agregar archivos de proyecto.

---
+++ NumerosPositivos/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace NumerosPositivos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPrimerValor = new System.Windows.Forms.TextBox();
+            this.txtSegundoValor = new System.Windows.Forms.TextBox();
+            this.btnCalculo = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(42, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(181, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese el primer valor:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(42, 166);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(197, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ingrese el segundo valor:";
+            // 
+            // txtPrimerValor
+            // 
+            this.txtPrimerValor.Location = new System.Drawing.Point(244, 115);
+            this.txtPrimerValor.Name = "txtPrimerValor";
+            this.txtPrimerValor.Size = new System.Drawing.Size(182, 20);
+            this.txtPrimerValor.TabIndex = 2;
+            // 
+            // txtSegundoValor
+            // 
+            this.txtSegundoValor.Location = new System.Drawing.Point(248, 169);
+            this.txtSegundoValor.Name = "txtSegundoValor";
+            this.txtSegundoValor.Size = new System.Drawing.Size(178, 20);
+            this.txtSegundoValor.TabIndex = 3;
+            // 
+            // btnCalculo
+            // 
+            this.btnCalculo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculo.Location = new System.Drawing.Point(150, 311);
+            this.btnCalculo.Name = "btnCalculo";
+            this.btnCalculo.Size = new System.Drawing.Size(89, 53);
+            this.btnCalculo.TabIndex = 4;
+            this.btnCalculo.Text = "Calcular diferencia";
+            this.btnCalculo.UseVisualStyleBackColor = true;
+            this.btnCalculo.Click += new System.EventHandler(this.btnCalculo_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(112, 245);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "El resultado es:";
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(254, 254);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(107, 20);
+            this.txtResultado.TabIndex = 6;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Location = new System.Drawing.Point(346, 311);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(89, 50);
+            this.btnSalir.TabIndex = 7;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(180, 30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(206, 24);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Calcular la diferencia";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(586, 451);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnCalculo);
+            this.Controls.Add(this.txtSegundoValor);
+            this.Controls.Add(this.txtPrimerValor);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPrimerValor;
+        private System.Windows.Forms.TextBox txtSegundoValor;
+        private System.Windows.Forms.Button btnCalculo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ NumerosPositivos/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NumerosPositivos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnCalculo_Click(object sender, EventArgs e)
+        {
+            double valor1 = 0;
+            double valor2 = 0;
+            double result = 0;
+
+            valor1 = Convert.ToDouble(txtPrimerValor.Text);
+            valor2 = Convert.ToDouble(txtSegundoValor.Text );
+
+            if(valor1 > valor2) 
+            {
+                result = valor1 - valor2;
+
+                txtResultado.Text = Convert.ToString(result);
+            }
+
+            if (valor2 > valor1)
+            {
+                result = valor2 - valor1;
+
+                txtResultado.Text = Convert.ToString(result);
+
+            }
+
+
+
+
+
+
+        }
+    }
+}
